4869ddafedd1e358e1ce2e62e6c1b02feb221ff4
Ioannis Mavrodimos <giannis@iluma.gr>
2026-09-11 21:37:50 +0300

    Fix Webpushr consent initialization

--- includes/cookiebanner.php
+++ includes/cookiebanner.php
@@ -25,7 +25,7 @@ function initWebpushr() {
         if(typeof(w.webpushr)!=='undefined') return;
         w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};
         var js, fjs = d.getElementsByTagName(s)[0];
-        js = d.createElement(s); js.id = id; js.async=1;
+        js = d.createElement(s); js.id = i; js.async=1;
         js.src = "https://cdn.webpushr.com/app.min.js";
         fjs.parentNode.insertBefore(js,fjs);
     }(window,document,'script','webpushr-jssdk'));
