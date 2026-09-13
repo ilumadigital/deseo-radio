2dc5601c71008313d7962f9c185f8fd5c00363f5
Ioannis Mavrodimos <giannis@iluma.gr>
2026-09-14 01:22:25 +0300

    Refresh PWA cache for install flow

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ importScripts('https://cdn.webpushr.com/sw-server.min.js');
    Static-assets-only caching. The live iRadios player is cross-origin and is
    intentionally never intercepted or cached by this service worker. */
 
-var CACHE_NAME = 'deseo-static-v6';
+var CACHE_NAME = 'deseo-static-v7';
 var STATIC_ASSETS = [
   '/offline.html',
   '/assets/img/favicon.png',
