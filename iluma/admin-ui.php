28f629f650223889816084781fc3f66a21c21c51
Ioannis Mavrodimos <support@iluma.gr>
2026-09-19 18:44:57 +0300

    Add Audience to ILUMA CMS navigation

--- iluma/admin-ui.php
+++ iluma/admin-ui.php
@@ -8,6 +8,7 @@ function admin_page_start(string $title, string $active = 'dashboard'): void {
         'program' => ['program.php', 'Radio Program'],
         'dj-season' => ['dj-season.php', 'Season 6 DJs'],
         'mylive' => ['mylive.php', 'MyLive'],
+        'audience' => ['audience.php', 'Audience'],
         'playlists' => ['playlists.php', 'Playlists'],
     ];
     ?>
