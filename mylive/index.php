12cc4e0ae94dac77a19b27128fc2ac6971be620c
Ioannis Mavrodimos <support@iluma.gr>
2026-09-23 17:01:50 +0300

    Remove Webpushr vendor opt-in loader from MyLive pages

--- mylive/index.php
+++ mylive/index.php
@@ -613,22 +613,6 @@ document.querySelectorAll('[data-password-toggle]').forEach(button => {
     });
 });
 </script>
-<!-- Webpushr · MyLive manual integration -->
-<!-- start webpushr code -->
-<script>
-(function(w,d,s,id){
-    if(typeof(w.webpushr)!=='undefined') return;
-    w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};
-    var js, fjs=d.getElementsByTagName(s)[0];
-    js=d.createElement(s);
-    js.id=id;
-    js.async=1;
-    js.src="https://cdn.webpushr.com/app.min.js";
-    fjs.parentNode.appendChild(js);
-}(window,document,'script','webpushr-jssdk'));
-webpushr('setup',{'key':'BBFQF4QEleCIF57CoJnUQRFeh6iQaAxCWSQoVibx6rYFQpavi8Z_2flBoHsn6r6SXV4udR__JwYH7ahY4-m4Lek'});
-</script>
-<!-- end webpushr code -->
 </body>
 </html>
 <?php
@@ -723,22 +707,6 @@ document.querySelectorAll('[data-password-toggle]').forEach(button => {
     });
 });
 </script>
-<!-- Webpushr · MyLive manual integration -->
-<!-- start webpushr code -->
-<script>
-(function(w,d,s,id){
-    if(typeof(w.webpushr)!=='undefined') return;
-    w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};
-    var js, fjs=d.getElementsByTagName(s)[0];
-    js=d.createElement(s);
-    js.id=id;
-    js.async=1;
-    js.src="https://cdn.webpushr.com/app.min.js";
-    fjs.parentNode.appendChild(js);
-}(window,document,'script','webpushr-jssdk'));
-webpushr('setup',{'key':'BBFQF4QEleCIF57CoJnUQRFeh6iQaAxCWSQoVibx6rYFQpavi8Z_2flBoHsn6r6SXV4udR__JwYH7ahY4-m4Lek'});
-</script>
-<!-- end webpushr code -->
 </body>
 </html>
 <?php
@@ -1783,21 +1751,5 @@ $nextShowMessage = match ($nextShowSetStatus) {
     });
 }());
 </script>
-<!-- Webpushr · MyLive manual integration -->
-<!-- start webpushr code -->
-<script>
-(function(w,d,s,id){
-    if(typeof(w.webpushr)!=='undefined') return;
-    w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};
-    var js, fjs=d.getElementsByTagName(s)[0];
-    js=d.createElement(s);
-    js.id=id;
-    js.async=1;
-    js.src="https://cdn.webpushr.com/app.min.js";
-    fjs.parentNode.appendChild(js);
-}(window,document,'script','webpushr-jssdk'));
-webpushr('setup',{'key':'BBFQF4QEleCIF57CoJnUQRFeh6iQaAxCWSQoVibx6rYFQpavi8Z_2flBoHsn6r6SXV4udR__JwYH7ahY4-m4Lek'});
-</script>
-<!-- end webpushr code -->
 </body>
 </html>
